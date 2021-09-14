ac053970f56bbae10d6d349c1e7b331885821960
husain-ujjaini <husainujjainwala78@gmail.com>
2021-09-15 00:08:06 +0530

    objects and arrays in javascript

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+let mydata = {
+    name: "husain",
+    country: "india",
+    role: "student"
+}
+let EveryData = [
+husain = {
+    name: "husain",
+    country: "india",
+    role: "student"
+},
+aman_sir = {
+    name: "aman",
+    country: "india",
+    role: "student"
+},
+murtaza_sir = {
+    name: "murtaza",
+    country: "india",
+    role: "teacher"
+},
+surbi_mam = {
+    name: "surbi",
+    country: "india",
+    role: "student"
+},
+sakshi_mam = {
+    name: "sakshi",
+    country: "india",
+    role: "student"
+},
+aayush_sir = {
+    name: "ayush",
+    country: "india",
+    role: "student"
+}
+]
+console.log(EveryData)
+console.log(math.PI)
