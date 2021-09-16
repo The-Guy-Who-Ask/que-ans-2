325a1b162e9192dd91ee8fc5c0795489aea9a87d
husain-ujjaini <husainujjainwala78@gmail.com>
2021-09-16 13:04:20 +0530

    this is basics of javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,37 +3,52 @@ let mydata = {
     country: "india",
     role: "student"
 }
+let string = "blah blah blah"
 let EveryData = [
-husain = {
-    name: "husain",
-    country: "india",
-    role: "student"
-},
-aman_sir = {
-    name: "aman",
-    country: "india",
-    role: "student"
-},
-murtaza_sir = {
-    name: "murtaza",
-    country: "india",
-    role: "teacher"
-},
-surbi_mam = {
-    name: "surbi",
-    country: "india",
-    role: "student"
-},
-sakshi_mam = {
-    name: "sakshi",
-    country: "india",
-    role: "student"
-},
-aayush_sir = {
-    name: "ayush",
-    country: "india",
-    role: "student"
-}
-]
-console.log(EveryData)
-console.log(math.PI)
+    husain = {
+        name: "husain",
+        country: "india",
+        role: "student"
+    },
+    aman_sir = {
+        name: "aman",
+        country: "india",
+        role: "student"
+    },
+    murtaza_sir = {
+        name: "murtaza",
+        country: "india",
+        role: "teacher"
+    },
+    surbi_mam = {
+        name: "surbi",
+        country: "india",
+        role: "student"
+    },
+    sakshi_mam = {
+        name: "sakshi",
+        country: "india",
+        role: "student"
+    },
+    aayush_sir = {
+        name: "ayush",
+        country: "india",
+        role: "student"
+    }
+ ]
+ let another_string = "Murtaza is awesome in programming"
+let slicy_string = "murtaza is a good teacher"
+let split_string = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+let replaced_string = "you will learn JavaScript"
+let small_word = "javascript"
+console.log(string.toUpperCase())
+console.log(string.toLowerCase())
+console.log(string.substr(1,2))
+console.log(string.substring(0,3))
+console.log(slicy_string.substr(13))
+console.log(another_string.split(" "))
+console.log(split_string.split(","))
+console.log(replaced_string.replace("javascript","python"))
+console.log(small_word.charAt(5))
+console.log(small_word.indexOf("a"))
+console.log(small_word.lastIndexOf("a"))
